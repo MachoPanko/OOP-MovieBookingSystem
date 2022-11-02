25ed0e36a3e52dd14877b770c9237caa074697cf
MachoPanko <64691592+MachoPanko@users.noreply.github.com>
2022-11-02 17:54:47 +0800

    holiday checker added

--- src/main/java/model/DayType.java
+++ src/main/java/model/DayType.java
@@ -5,19 +5,27 @@ import java.util.*;
 
 
 
-public enum DayType {
+public enum DayType {  /// MUST INITIALIZE WITH yyyy-MM-dd format
 
     Weekday("Weekday"),
     Weekend("Weekdend"),
     Holiday("Holiday");
 
     private Date day;
-    DayType(String date) {// in hyphen separated format
+    private String dayString;
+    private ArrayList<String> holidayDates = new ArrayList<String>(List.of("0101","0102","0202","1504","0205","0305","1605","1107","0908","2410","2612"));
+    DayType(String date) {// in hyphen separated format yyyy-MM-dd
         day = parseDate(date);
+        dayString =   date.substring(8) + date.substring(5,7);
     }
     public DayType getType(){
         Calendar c1 = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
-        c1.setTime(day);;
+        c1.setTime(day);
+        for ( String holidays : holidayDates){
+            if ( dayString.equals(holidays)){
+                return Holiday;
+            }
+        }
         if (c1.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY || c1.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY ){
             return Weekend;
         }

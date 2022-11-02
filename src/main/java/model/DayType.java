044111b7e9049f2d544f2e24670a04e4a5379f49
MachoPanko <64691592+MachoPanko@users.noreply.github.com>
2022-11-02 18:42:43 +0800

    fixed Daytype enum

--- src/main/java/model/DayType.java
+++ src/main/java/model/DayType.java
@@ -7,30 +7,34 @@ import java.util.*;
 
 public enum DayType {  /// MUST INITIALIZE WITH yyyy-MM-dd format
 
-    Weekday("Weekday"),
-    Weekend("Weekdend"),
-    Holiday("Holiday");
+    WEEKDAY("Weekday"),
+    WEEKEND("Weekdend"),
+    HOLIDAY("Holiday");
 
-    private Date day;
-    private String dayString;
-    private ArrayList<String> holidayDates = new ArrayList<String>(List.of("0101","0102","0202","1504","0205","0305","1605","1107","0908","2410","2612"));
-    DayType(String date) {// in hyphen separated format yyyy-MM-dd
+    private String day1;
+    private static Date day;
+    private static String dayString;
+    private static ArrayList<String> holidayDates = new ArrayList<String>(List.of("0101","0102","0202","1504","0205","0305","1605","1107","0908","2410","2612"));
+
+    DayType(String day) {
+        this.day1=day;
+    }
+
+    public static DayType getType(String date){
         day = parseDate(date);
         dayString =   date.substring(8) + date.substring(5,7);
-    }
-    public DayType getType(){
         Calendar c1 = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
         c1.setTime(day);
         for ( String holidays : holidayDates){
             if ( dayString.equals(holidays)){
-                return Holiday;
+                return HOLIDAY;
             }
         }
         if (c1.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY || c1.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY ){
-            return Weekend;
+            return WEEKEND;
         }
         else {
-            return Weekday;
+            return WEEKDAY;
         }
     }
     private static Date parseDate(String date) {

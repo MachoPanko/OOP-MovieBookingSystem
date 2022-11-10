fb5e07789e3a80e7b0349db9428de296e8b6736c
BudiNverse <me@inve.rs>
2022-11-10 18:00:48 +0800

    Simplified UserLogin Simplified loadStaff

--- src/main/java/view/UserLogin.java
+++ src/main/java/view/UserLogin.java
@@ -3,66 +3,61 @@ package view;
 
 import controller.DatabaseController;
 import model.classes.Staff;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UserLogin {
 
-
-    public static void login(){
+    public static void login() {
         System.out.println("Welcome to MOBLIMA");
         System.out.println("Are you a staff or a customer?");
         System.out.println("1) Staff\n2) Customer");
 
         Scanner sc = new Scanner(System.in);
-        int choice = sc.nextInt();
-        sc.nextLine();
-        switch (choice){
-            case 1:
+        int choice = Integer.parseInt(sc.nextLine());
+        switch (choice) {
+            case 1 -> {
                 System.out.println("You are a staff!");
                 System.out.println("1) Create staff account\n2) Login staff account");
                 int choiceToCreateOrLogin = sc.nextInt();
                 sc.nextLine();
-                Object DatabaseController;
-                if(choiceToCreateOrLogin == 1){ // Create a new staff account
-                    System.out.println("Create your username");
-                    String username = sc.nextLine();
+                switch (choiceToCreateOrLogin) {
+                    case 1 -> { // Create a new staff account
+                        System.out.println("Create your username");
+                        String username = sc.nextLine();
 
-                    System.out.println("Create your password");
-                    String password = sc.nextLine();
+                        System.out.println("Create your password");
+                        String password = sc.nextLine();
 
-                    Staff staff =  new Staff(username, password);
-                    controller.DatabaseController.saveStaffAccount(staff);
-                }
-                else{ // Login to an existing account
-                    System.out.println("Enter your username");
-                    String username = sc.nextLine();
+                        Staff staff = new Staff(username, password);
+                        DatabaseController.saveStaffAccount(staff);
+                    }
+                    case 2 -> { // Login to an existing account
+                        System.out.println("Enter your username");
+                        String username = sc.nextLine();
 
-                    System.out.println("Enter your password");
-                    String password = sc.nextLine();
-
-                    ArrayList<Staff> staff = controller.DatabaseController.loadStaffAccount();
-                    for(Staff s : staff){
-                        if(s.getUsername().toUpperCase().equals(username.toUpperCase()) &&
-                                s.getPassword().toUpperCase().equals(password.toUpperCase())){
-                            System.out.println("Welcome "+ s.getUsername());
-                            System.out.println("Enter your choice");
-                            System.out.println("1) Configure movies\n");
-                            int staffChoice = sc.nextInt();
-                            sc.nextLine();
-                            if(staffChoice == 1){
-                                MovieConfiguration.configureMovies();
+                        System.out.println("Enter your password");
+                        String password = sc.nextLine();
+                        ArrayList<Staff> staff = DatabaseController.loadStaffAccount();
+                        for (Staff s : staff) {
+                            if (s.getUsername().equalsIgnoreCase(username) &&
+                                    s.getPassword().equalsIgnoreCase(password)) {
+                                System.out.println("Welcome " + s.getUsername());
+                                System.out.println("Enter your choice");
+                                System.out.println("1) Configure movies\n");
+                                int staffChoice = Integer.parseInt(sc.nextLine());
+                                if (staffChoice == 1) {
+                                    MovieConfiguration.configureMovies();
+                                }
+                            } else {
+                                System.out.println("The details you have entered does not exist");
                             }
-                            break;
-                        }
-                        else{
-                            System.out.println("The details you have entered does not exist");
                             break;
                         }
                     }
                 }
-
-
+            }
         }
     }
 

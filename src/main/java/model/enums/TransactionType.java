3f2ab7705aca6ab5a1670768a9e94f9ec02692a0
MachoPanko <64691592+MachoPanko@users.noreply.github.com>
2022-11-10 18:04:23 +0800

    Update booking Testing,  need to fill in all the parameters

--- src/main/java/model/enums/TransactionType.java
+++ src/main/java/model/enums/TransactionType.java
@@ -1,4 +1,14 @@
 package model.enums;
 
+import model.classes.Transaction;
+
 public enum TransactionType {
+    TEST("TEST");
+
+    TransactionType(String test) {
+    }
+
+    public static TransactionType getType() {
+        return TEST;
+    }
 }

1f1c747ac48e0bfff928d8de804fe1ea1e09458f
BudiNverse <me@inve.rs>
2022-11-10 18:24:15 +0800

    Added cinemaList to cineplex Cleaned up MovieTicket

--- src/main/java/model/classes/Cineplex.java
+++ src/main/java/model/classes/Cineplex.java
@@ -1,35 +1,32 @@
 package model.classes;
+
 import java.util.*;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Cineplex {
     private int cineplexID;
-    private ArrayList<Movie> movieList;
 
-    Cineplex(int cineplexID){
+    private final ArrayList<Movie> movieList;
+    private final ArrayList<Cinema> cinemaList;
+
+    Cineplex(int cineplexID) {
         this.cineplexID = cineplexID;
         movieList = new ArrayList<>();
+        cinemaList = new ArrayList<>();
     }
-    Cineplex(int cineplexID , ArrayList<Movie> movieList){
+
+    Cineplex(int cineplexID, ArrayList<Movie> movieList, ArrayList<Cinema> cinemaList) {
         this.cineplexID = cineplexID;
         this.movieList = movieList;
-    }
-    public boolean addMovie (Movie newMovie){
-        return this.movieList.add(newMovie);
-    }
-    public boolean removeMoviebyIndex(int index){
-        return this.movieList.remove(index) != null;
-    }
-    public boolean removeMoviebyMovie(Movie movie){
-        return this.movieList.remove(movie);
+        this.cinemaList = cinemaList;
     }
 
-    public int getCineplexID() {
-        return cineplexID;
-    }
-
-    public void setCineplexID(int cineplexID) {
-        this.cineplexID = cineplexID;
-    }
+    public boolean addMovie(Movie newMovie) { return this.movieList.add(newMovie); }
+    public boolean removeMovieByIndex(int index) { return this.movieList.remove(index) != null; }
+    public boolean removeMovieByMovie(Movie movie) { return this.movieList.remove(movie); }
+    public ArrayList<Cinema> getCinemaList() { return this.cinemaList; }
+    public int getCineplexID() { return cineplexID; }
+    public ArrayList<Movie> getMovieList() { return movieList; }
+    public void setCineplexID(int cineplexID) { this.cineplexID = cineplexID; }
 }

aab3a92565fb5d03738e73d5dbac77f752014dc4
BudiNverse <me@inve.rs>
2022-11-11 16:37:07 +0800

    Updated MovieController

--- src/main/java/controller/MovieController.java
+++ src/main/java/controller/MovieController.java
@@ -8,10 +8,14 @@ import java.util.HashMap;
 public class MovieController {
     private static final String filename = "src/database/movieDatabase.txt";
 
-    public static HashMap<String, Movie> MOVIES = new HashMap<>();
+    public static final HashMap<String, Movie> MOVIES = new HashMap<>();
 
     public static void loadMovies() {
         HashMap<String, Movie> hm = DatabaseLoader.loadDb(filename);
-        if(hm != null) MOVIES = hm;
+        if(hm != null) MOVIES.putAll(hm);
+    }
+
+    public static void writeMovies() {
+        DatabaseLoader.writeDb(filename, MOVIES);
     }
 }

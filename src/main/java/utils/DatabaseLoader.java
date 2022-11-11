6338227bf6ddaf6c5acbedb07e6d52625114b6eb
BudiNverse <me@inve.rs>
2022-11-11 16:28:20 +0800

    Added DatabaseLoader

---
+++ src/main/java/utils/DatabaseLoader.java
@@ -0,0 +1,28 @@
+package utils;
+import java.io.*;
+
+public class DatabaseLoader {
+
+    public static <T> T loadDb(String filename)  {
+        T result = null;
+
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename));
+            result = (T) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return result;
+    }
+
+    public static <T> void writeDb(String filename, T buffer) {
+        try{
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename));
+            oos.writeObject(buffer);
+        }catch(Exception e){
+            System.out.println("Error writing to " + filename);
+            System.out.println(e.getMessage());
+        }
+    }
+}

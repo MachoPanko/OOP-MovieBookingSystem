9979fb9a7b6e11ce9c39cd324ebd4ef57f96cb1c
MachoPanko <64691592+MachoPanko@users.noreply.github.com>
2022-11-12 01:00:06 +0800

    Wrote MovieGoerLogin

--- src/main/java/view/MovieGoerLogin.java
+++ src/main/java/view/MovieGoerLogin.java
@@ -1,7 +1,35 @@
 package view;
+import model.classes.MovieGoer;
+import static model.Main.SC;
+import static model.Main.VIEW_STATE;
 
 public class MovieGoerLogin {
     public static void display() {
+        System.out.println("Guest login!");
+        System.out.println("1) Confirm and Proceed\n2) Back");
+        int choice =  SC.nextInt();
+        SC.nextLine();
+
+        if (choice == 1) {// Create a new staff account
+            System.out.println("Hello Guest! Guest account creation in progress...");
+            System.out.println("Please enter your username");
+            String username = SC.nextLine();
+            System.out.println("Please enter your name");
+            String name = SC.nextLine();
+            System.out.println("Please enter your mobile number");
+            int mobile = SC.nextInt();
+            SC.nextLine();
+            System.out.println("Please enter your age");
+            int age = SC.nextInt();
+            SC.nextLine();
+            System.out.println("Please enter your email");
+            String email = SC.nextLine();
+            VIEW_STATE.setCurrState(ViewState.State.MovieGoerView);
+            VIEW_STATE.setCurrUser(new MovieGoer(username,name,mobile,age,email));
+
+        } else {
+            VIEW_STATE.setCurrState(ViewState.State.LoginView);
+        }
 
     }
 }

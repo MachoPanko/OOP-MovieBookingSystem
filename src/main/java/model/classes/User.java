9e64f337eb4e8ad5e1d31d41111b3fe345f9d825
irfaan <irfaan002@e.ntu.edu.sg>
2022-11-13 22:01:03 +0800

    Javadoc for user interface

--- src/main/java/model/classes/User.java
+++ src/main/java/model/classes/User.java
@@ -1,7 +1,7 @@
 package model.classes;
 
 /**
- * This class is used by moviegoer and staff class and forces them to implement the method getUsername
+ * This class is inherited by moviegoer and staff class and forces them to implement the method getUsername
  */
 public interface User {
 
